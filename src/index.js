1cf1ca8516828f6ef45e55b7d35cad693fb61183
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-11 14:36:19 -0500

    intialized

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App/>, rootElement);
